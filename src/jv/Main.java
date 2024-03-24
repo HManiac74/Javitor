227e80387efc78205e6ed15134c8116e755e50ed
HManiac74 <hmaniac28@yahoo.de>
2024-03-24 11:25:03 +0100

    Initial commit, minimum GUI

---
+++ src/jv/Main.java
@@ -0,0 +1,14 @@
+package jv;
+
+import javax.swing.JTextPane;
+
+public class Main extends JTextPane{
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		new UI().setVisible(true);
+
+	}
+
+}
