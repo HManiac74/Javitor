e2b1d349e8e31f941a2f08cde0c1bf09cd2b75e7
HManiac74 <hmaniac28@yahoo.de>
2024-03-24 11:36:09 +0100

    add untitled TextArea

--- src/jv/UI.java
+++ src/jv/UI.java
@@ -1,13 +1,33 @@
 package jv;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class UI extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+	private Container container;
+	private JTextArea textArea;
+	
+	public UI() {
+		container = getContentPane();
+		
+		setSize(500, 300);
+		setTitle("Untitled");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		textArea = new JTextArea("", 0, 0);
+		textArea.setFont(new Font("Century Gothic", Font.BOLD, 12));
+		
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(textArea);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
